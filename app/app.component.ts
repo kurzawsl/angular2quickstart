2c29b2a8ba57724e915a833c9ae0c19d60908c12
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-01-25 20:59:42 +0100

    Change Angular to John Doe and display emial

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+          <p>{{email}}</p>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'John Doe';
+  email = 'john.doe@gmail.com';
+}

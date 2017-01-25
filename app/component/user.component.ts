342431fc4737cf3a4e8942156d9dd03e0b32957e
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-01-25 22:20:13 +0100

    Adding and deleting hobbies

--- app/component/user.component.ts
+++ app/component/user.component.ts
@@ -9,10 +9,14 @@ import { Component } from '@angular/core';
           <div *ngIf="showHobbies">
           <h3>Hobbies</h3>
           <ul>
-            <li *ngFor="let hobby of hobbies">
-            {{hobby}}
+            <li *ngFor="let hobby of hobbies; let i = index">
+            {{hobby}}<button (click)="deleteHobby(i)">X</button>
             </li>
           </ul>
+          <form (submit)="addHobby(hobby.value)">
+            <label>Add Hobby: </label><br />
+            <input type="text" #hobby /><br />
+          </form>
           </div>
           <h3>Edit User</h3>
           <form>
@@ -55,6 +59,14 @@ export class UserComponent {
     } else {
       this.showHobbies = true;
     }
+  }
+
+  addHobby(hobby: string){
+    this.hobbies.push(hobby);
+  }
+
+  deleteHobby(i: number){
+    this.hobbies.splice(i,1);
   }
 
 }
